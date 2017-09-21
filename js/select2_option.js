b9411194682a56b7bfa147a5f8d00df40404683e
sun <sunzhanzhuang@qtone.cn>
2017-09-21 15:32:26 +0800

    9.21版本，有很多没有完善的地方

---
+++ js/select2_option.js
@@ -0,0 +1,30 @@
+$(function(){
+	
+	$(".js-select125").select2({
+		width: "1.25rem",
+		minimumResultsForSearch: Infinity
+	});
+	
+	$(".js-select133").select2({
+		width: "1.33rem",
+		minimumResultsForSearch: Infinity
+	});
+	
+	$(".js-select160").select2({
+  		width: "1.6rem",
+	  	minimumResultsForSearch: Infinity
+	});
+	
+	$(".js-select180").select2({
+  		width: "1.8rem",
+	  	minimumResultsForSearch: Infinity
+	});
+	$(".js-select285").select2({
+  		width: "2.85rem",
+	  	minimumResultsForSearch: Infinity
+	});
+	$(".js-select330").select2({
+  		width: "3.3rem",
+	  	minimumResultsForSearch: Infinity
+	});
+});
